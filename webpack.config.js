39149995fc5627a094d56683a7b8f5f125e88516
Jairo Valderrama <jairovg@users.noreply.github.com>
2018-02-18 13:40:53 -0500

    Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: './src/app.es6',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name]-bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.es6$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+          'eslint-loader'
+        ]
+      },
+      {
+        test: /\.vue$/,
+        exclude: /node_modules/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            js: 'babel-loader!eslint-loader',
+            scss: 'vue-style-loader!css-loader!sass-loader'
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.es6', '.vue']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src'),
+    historyApiFallback: true,
+    port: 3000,
+    compress: false,
+    inline: true,
+    hot: true,
+    stats: {
+      assets: false,
+      children: false,
+      chunks: false,
+      hash: false,
+      modules: false,
+      publicPath: false,
+      timings: false,
+      version: false,
+      warnings: true,
+      colors: {
+        green: '\u001b[32m',
+      }
+    }
+  }
+};
