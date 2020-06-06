b371367014e7e25ac2b355852ab4c44e68cc3143
Jairo Valderrama <jairovg@gmail.com>
2020-06-06 15:30:02 -0500

    Add login view

---
+++ src/components/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login">
+      <h1>Login</h1>
+      <div>
+        <h4>Email: </h4>
+        <input type="textfield" name="email" class="email" />
+      </div>
+      <div>
+        <h4>Password: </h4>
+        <input type="password" name="password" class="password" />
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  
+};
+</script>
