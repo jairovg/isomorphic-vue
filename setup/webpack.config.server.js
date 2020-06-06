91380fcc0ae05385009b54a7d524b3e25ef27a7e
Jairo Valderrama <jairovg@gmail.com>
2020-06-06 18:06:20 -0500

    Setup VueSSRServerPlugin in server config
    The server config is meant for generating the server bundle that will be passed to createBundleRenderer

--- setup/webpack.config.server.js
+++ setup/webpack.config.server.js
@@ -1,6 +1,7 @@
 const merge = require('webpack-merge');
 const nodeExternals = require('webpack-node-externals');
 const baseConfig = require('./webpack.config.base');
+const VueSSRServerPlugin = require('vue-server-renderer/server-plugin');
 
 module.exports = merge(baseConfig, {
   entry: './src/entry-server.es6',
@@ -16,7 +17,7 @@ module.exports = merge(baseConfig, {
   // This tells the server bundle to use Node-style exports
   output: {
     filename: 'server-bundle.js',
-    libraryTarget: 'commonjs2'
+    libraryTarget: 'commonjs2',
   },
 
   // https://webpack.js.org/configuration/externals/#function
@@ -27,13 +28,13 @@ module.exports = merge(baseConfig, {
     // do not externalize dependencies that need to be processed by webpack.
     // you can add more file types here e.g. raw *.vue files
     // you should also whitelist deps that modifies `global` (e.g. polyfills)
-    whitelist: /\.css$/
-  })//,
+    whitelist: /\.css$/,
+  }),
 
   // This is the plugin that turns the entire output of the server build
   // into a single JSON file. The default file name will be
   // `vue-ssr-server-bundle.json`
-  //plugins: [
-  //  new VueSSRServerPlugin()
-  //]
+  plugins: [
+    new VueSSRServerPlugin(),
+  ],
 });
